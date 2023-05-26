b3e857342c5104680da63409a4a39f09ae4fc22b
Yash Jasani <yashjasani@Yashs-MacBook-Air.local>
2023-05-26 14:01:38 +0530

    api features - search,filter,pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModels");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures")
 //Create Product -- admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
     
@@ -15,11 +16,19 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 //get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res)=>{
 
-    const products = await Product.find();
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+    const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
-        products
+        products,
+        productCount
     });
 });
 
---
+++ backend/utils/apiFeatures.js
@@ -0,0 +1,48 @@
+class ApiFeatures {
+    constructor(query, queryStr){
+        this.query = query;
+        this.queryStr = queryStr;
+    }
+
+    search(){
+        const keyword = this.queryStr.keyword ? {
+            name:{
+                $regex: this.queryStr.keyword,
+                $options:"i" //case insensitive 
+            }
+        }:{};
+
+        this.query = this.query.find({...keyword});
+        return this;
+    }
+
+    filter(){
+        const queryStrCopy = {...this.queryStr};
+
+        //removing some fields for category
+
+        const removeFields = ["keyword","page","limit",];
+        removeFields.forEach(key=>delete queryStrCopy[key]);
+
+        //Filter for price and Rating 
+
+        let queryStr = JSON.stringify(queryStrCopy);
+        console.log(queryStr);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, '$$' + "$1");
+
+
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    pagination(resultPerPage){
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resultPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+        return this;
+    }
+}
+
+module.exports = ApiFeatures
